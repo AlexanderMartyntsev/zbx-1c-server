f938eea056681ced0bfac68cd162aa872757111d
AlexanderMartyntsev <59194954+AlexanderMartyntsev@users.noreply.github.com>
2020-02-28 11:21:16 +0500

    Update scCreator.bat

--- scCreator.bat
+++ scCreator.bat
@@ -6,7 +6,7 @@ set CtrlPort=1540
 set AgentName=localhost
 set RASPort=1545
 set SrvcName="1C:Enterprise 8.3 RAS"
-set BinPath="\"C:\Program Files\1cv8\8.3.16.1148\bin\ras.exe\" cluster --service --port= %RASPort% %AgentName%:%CtrlPort%"
+set BinPath="\"C:\Program Files\1cv8\8.3.16.1148\bin\ras.exe\" cluster --service --port=%RASPort% %AgentName%:%CtrlPort%"
 set Desctiption="1C:Enterprise 8.3 RAS"
 sc stop %SrvcName%
 sc delete %SrvcName%
