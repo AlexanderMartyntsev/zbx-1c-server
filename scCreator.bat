fb88da8488f070b2a66c2ded61ffbe0351079d81
AlexanderMartyntsev <59194954+AlexanderMartyntsev@users.noreply.github.com>
2020-02-28 10:59:15 +0500

    Create scCreator.bat

---
+++ scCreator.bat
@@ -0,0 +1,13 @@
+@echo off
+set Version="8.3.16.1030"
+set SrvUserName=.\USR1CV8
+set SrvUserPwd="password"
+set CtrlPort=1540
+set AgentName=localhost
+set RASPort=1545
+set SrvcName="1C:Enterprise 8.3 RAS"
+set BinPath="\"C:\Program Files\1cv8\%Version%\bin\ras.exe\" cluster --service --port=%RASPort% %AgentName%:%CtrlPort%"
+set Desctiption="1C:Enterprise 8.3 RAS"
+sc stop %SrvcName%
+sc delete %SrvcName%
+sc create %SrvcName% binPath= %BinPath% start= auto obj= %SrvUserName% password= %SrvUserPwd% displayname= %Desctiption%
